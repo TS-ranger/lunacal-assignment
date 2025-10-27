e13c40d1b7e8c0f4c2068b9040f24a5ec91eee7e
Tushar Saha <sahatushar242002@gmail.com>
2025-10-27 10:35:46 +0530

    ui upd #4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,9 @@ export default function Home() {
   return (
     <main className="min-h-screen text-white p-4 md:p-8 flex items-center justify-center bg-linear-to-b from-[#2c3136] to-[#191B1F]">
       <div className="w-full max-w-7xl grid grid-cols-1 lg:grid-cols-2 gap-8">
-        <div>Left Div</div>
+        <div>
+          
+        </div>
 
         <div className="flex flex-col space-y-8">
           <ProfileWidget />
